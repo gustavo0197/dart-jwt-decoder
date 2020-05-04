1a2ac0918fe6d7670c27ae96762966845e0cada2
Gustavo Velazquez <tavoo_velazquez@hotmail.com>
2020-05-04 14:29:17 -0500

    Version 1.0.0

---
+++ lib/jwt_decoder.dart
@@ -0,0 +1,44 @@
+library jwt_decoder;
+
+import 'dart:convert';
+
+class JwtDecoder {
+  static Map<String, dynamic> decode(String token) {
+    try {
+      List<String> splitToken = token.split("."); // Split the token by '.'
+      String payloadBase64 = splitToken[1]; // Payload is always the index 1
+      // Base64 should be multiple of 4. Normalize the payload before decode it
+      String normalizedPayload = base64.normalize(payloadBase64);
+      // Decode payload, the result is a String
+      String payloadString = utf8.decode(base64.decode(normalizedPayload));
+      // Parse the String to a Map<String, dynamic>
+      Map<String, dynamic> decodedPayload = jsonDecode(payloadString);
+
+      // Return the decoded payload
+      return decodedPayload;
+    } catch (error) {
+      // If there's an error return null
+      return null;
+    }
+  }
+
+  static bool isExpired(String token) {
+    try {
+      // Decode the token
+      final Map<String, dynamic> decodedToken = decode(token);
+      if (decodedToken != null) {
+        // Get the expiration date
+        final DateTime expirationDate =
+            new DateTime.fromMillisecondsSinceEpoch(0)
+                .add(new Duration(seconds: decodedToken["exp"]));
+        // If the current date is after the expiration date, the token is already expired
+        return new DateTime.now().isAfter(expirationDate);
+      } else {
+        return true;
+      }
+    } catch (error) {
+      print(error);
+      return true;
+    }
+  }
+}
---
+++ test/jwt_decoder_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+
+const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikd1c3Rhdm8iLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6NDczNDYxNTg1OH0.hh-TTBPS8z-UxdmfXWn7AwW2y_Lq3aPnlIQdqV2KEC4";
+const expiredToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MjYyMzkwMjJ9.GMdV0dx1F8rZuHUebeXL5tR2DROlc03IuDc2DeDTExI";
+
+void main() {
+  test("Decode a valid token", () {
+    expect(JwtDecoder.decode(token)["name"], "Gustavo");
+  });
+
+  test("Decode an invalid token", () {
+    expect(JwtDecoder.decode(""), null);
+  });
+
+  test("isExpired? Valid and no expired token", () {
+    expect(JwtDecoder.isExpired(token), false);
+  });
+
+  test("isExpired? Valid but expired token", () {
+    expect(JwtDecoder.isExpired(expiredToken), true);
+  });
+
+  test("isExpired? Invalid token", () {
+    expect(JwtDecoder.isExpired("lñaslksa"), true);
+  });
+}
