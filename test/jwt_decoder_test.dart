c66081f09cd50ebf1febae9f039beca9f83cf47b
Gustavo <gustavo.velazquezm@outlook.com>
2020-10-19 22:49:37 -0500

    Updated tests

--- test/jwt_decoder_test.dart
+++ test/jwt_decoder_test.dart
@@ -28,7 +28,6 @@ void main() {
   });
 
   test("Expiration date", () {
-    print(JwtDecoder.getExpirationDate(token));
     expect(
         JwtDecoder.getExpirationDate(token).isAfter(new DateTime.now()), true);
   });
