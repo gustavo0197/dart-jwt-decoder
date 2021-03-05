ed3e1df79f72120928ff40d80add101c9342359c
Olivier Revial <olivier.revial@stack-labs.com>
2021-03-05 11:26:21 +0100

    Migrate to Dart null safety system

--- lib/jwt_decoder.dart
+++ lib/jwt_decoder.dart
@@ -3,66 +3,77 @@ library jwt_decoder;
 import 'dart:convert';
 
 class JwtDecoder {
+  /// Decode a string JWT token into a `Map<String, dynamic>`
+  /// containing the decoded JSON payload.
+  ///
+  /// Note: header and signature are not returned by this method.
+  ///
+  /// Throws [FormatException] if parameter is not a valid JWT token.
   static Map<String, dynamic> decode(String token) {
+    final splitToken = token.split("."); // Split the token by '.'
+    if (splitToken.length != 3) {
+      throw FormatException('Invalid token');
+    }
     try {
-      List<String> splitToken = token.split("."); // Split the token by '.'
-      String payloadBase64 = splitToken[1]; // Payload is always the index 1
+      final payloadBase64 = splitToken[1]; // Payload is always the index 1
       // Base64 should be multiple of 4. Normalize the payload before decode it
-      String normalizedPayload = base64.normalize(payloadBase64);
+      final normalizedPayload = base64.normalize(payloadBase64);
       // Decode payload, the result is a String
-      String payloadString = utf8.decode(base64.decode(normalizedPayload));
+      final payloadString = utf8.decode(base64.decode(normalizedPayload));
       // Parse the String to a Map<String, dynamic>
-      Map<String, dynamic> decodedPayload = jsonDecode(payloadString);
+      final decodedPayload = jsonDecode(payloadString);
 
       // Return the decoded payload
       return decodedPayload;
     } catch (error) {
-      // If there's an error return null
-      return null;
+      throw FormatException('Invalid payload');
     }
   }
 
-  static bool isExpired(String token) {
+  /// Decode a string JWT token into a `Map<String, dynamic>`
+  /// containing the decoded JSON payload.
+  ///
+  /// Note: header and signature are not returned by this method.
+  ///
+  /// Returns null if the token is not valid
+  static Map<String, dynamic>? tryDecode(String token) {
     try {
-      // Decode the token
-      final Map<String, dynamic> decodedToken = decode(token);
-      if (decodedToken != null) {
-        // Get the expiration date
-        //exp must convert to integer because after decode will be respone double
-        final DateTime expirationDate =
-            new DateTime.fromMillisecondsSinceEpoch(0)
-                .add(new Duration(seconds: decodedToken["exp"].toInt()));
-        // If the current date is after the expiration date, the token is already expired
-        return new DateTime.now().isAfter(expirationDate);
-      } else {
-        return true;
-      }
+      return decode(token);
     } catch (error) {
-      return true;
+      return null;
     }
   }
 
+  /// Tells whether a token is expired.
+  ///
+  /// Returns true if the token is valid, false if it is expired.
+  ///
+  /// Throws [FormatException] if parameter is not a valid JWT token.
+  static bool isExpired(String token) {
+    final expirationDate = getExpirationDate(token);
+    // If the current date is after the expiration date, the token is already expired
+    return DateTime.now().isAfter(expirationDate);
+  }
+
+  /// Returns token expiration date
+  ///
+  /// Throws [FormatException] if parameter is not a valid JWT token.
   static DateTime getExpirationDate(String token) {
-    final Map<String, dynamic> decodedToken = decode(token);
+    final decodedToken = decode(token);
 
-    if (decodedToken != null) {
-      final DateTime expirationDate = new DateTime.fromMillisecondsSinceEpoch(0)
-          .add(new Duration(seconds: decodedToken['exp'].toInt()));
-      return expirationDate;
-    } else {
-      return null;
-    }
+    final expirationDate = DateTime.fromMillisecondsSinceEpoch(0)
+        .add(Duration(seconds: decodedToken['exp'].toInt()));
+    return expirationDate;
   }
 
+  /// Returns token issuing date (iat)
+  ///
+  /// Throws [FormatException] if parameter is not a valid JWT token.
   static Duration getTokenTime(String token) {
-    final Map<String, dynamic> decodedToken = decode(token);
+    final decodedToken = decode(token);
 
-    if (decodedToken != null) {
-      final DateTime expirationDate = new DateTime.fromMillisecondsSinceEpoch(0)
-          .add(new Duration(seconds: decodedToken["iat"]));
-      return new DateTime.now().difference(expirationDate);
-    } else {
-      return null;
-    }
+    final issuedAtDate = DateTime.fromMillisecondsSinceEpoch(0)
+        .add(Duration(seconds: decodedToken["iat"]));
+    return DateTime.now().difference(issuedAtDate);
   }
 }
--- test/jwt_decoder_test.dart
+++ test/jwt_decoder_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:test/test.dart';
 
 const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikd1c3Rhdm8iLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6NDczNDYxNTg1OH0.hh-TTBPS8z-UxdmfXWn7AwW2y_Lq3aPnlIQdqV2KEC4";
@@ -7,12 +7,40 @@ const expiredToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MjYyMzkwMjJ9.GMdV0dx1F8rZuHUebeXL5tR2DROlc03IuDc2DeDTExI";
 
 void main() {
-  test("Decode a valid token", () {
-    expect(JwtDecoder.decode(token)["name"], "Gustavo");
+  group('Decode', () {
+    test("a valid token", () {
+      expect(JwtDecoder.decode(token)["name"], "Gustavo");
+    });
+
+    test("an invalid token", () {
+      expect(
+        () => JwtDecoder.decode(""),
+        throwsA(isA<FormatException>()
+            .having((e) => e.message, 'message', 'Invalid token')),
+      );
+    });
+
+    test("an invalid payload", () {
+      expect(
+        () => JwtDecoder.decode("a.b.c"),
+        throwsA(isA<FormatException>()
+            .having((e) => e.message, 'message', 'Invalid payload')),
+      );
+    });
   });
 
-  test("Decode an invalid token", () {
-    expect(JwtDecoder.decode(""), null);
+  group('Try decode', () {
+    test("a valid token", () {
+      expect(JwtDecoder.tryDecode(token)!["name"], "Gustavo");
+    });
+
+    test("an invalid token", () {
+      expect(JwtDecoder.tryDecode(""), isNull);
+    });
+
+    test("an invalid payload", () {
+      expect(JwtDecoder.tryDecode("a.b.c"), isNull);
+    });
   });
 
   test("isExpired? Valid and no expired token", () {
@@ -24,7 +52,11 @@ void main() {
   });
 
   test("isExpired? Invalid token", () {
-    expect(JwtDecoder.isExpired("lñaslksa"), true);
+    expect(
+      () => JwtDecoder.isExpired("lñaslksa"),
+      throwsA(isA<FormatException>()
+          .having((e) => e.message, 'message', 'Invalid token')),
+    );
   });
 
   test("Expiration date", () {
@@ -32,7 +64,23 @@ void main() {
         JwtDecoder.getExpirationDate(token).isAfter(new DateTime.now()), true);
   });
 
+  test("Expiration date with invalid token", () {
+    expect(
+      () => JwtDecoder.getExpirationDate("an.invalid.payload"),
+      throwsA(isA<FormatException>()
+          .having((e) => e.message, 'message', 'Invalid payload')),
+    );
+  });
+
   test("Expiration time", () {
     expect(JwtDecoder.getTokenTime(token).inDays, greaterThan(1));
+  });
+
+  test("Expiration time with invalid token", () {
+    expect(
+      () => JwtDecoder.getTokenTime("invalid.token"),
+      throwsA(isA<FormatException>()
+          .having((e) => e.message, 'message', 'Invalid token')),
+    );
   });
 }
